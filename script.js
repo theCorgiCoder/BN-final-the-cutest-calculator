6b0f3a4f3830b4d7b43bf5fb427ad6effd3dbed5
Elaine B <elainebergstrom26@gmail.com>
2023-03-19 10:51:57 +0100

    updated numberInput and useOperators to display numbers properly

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const display = document.getElementById("display");
 
 let displayValue = "0";
 let result = null;
-let operationType = null;
+let number1 = null;
+let number2 = null;
+let operation1 = null;
+let operation2 = null;
 
 function updateDisplay() {
   // display.innerText = parseFloat(displayValue).toFixed(6);
@@ -54,35 +57,85 @@ function onClickHandler(button) {
 
 function inputNumbers(num) {
   //handles first input
-  if (operationType === null) {
+  if (operation1 === null) {
     if (displayValue === "0" || displayValue === 0) {
       displayValue = num;
-      console.log("num input1", num);
-    } else if (displayValue === num) {
-      displayValue = num;
-      console.log("num input2", num);
+      number1 = displayValue;
+      console.log(
+        "num input3",
+        num,
+        "n1: ",
+        number1,
+        "n2: ",
+        number2,
+        displayValue
+      );
+    } else if (displayValue === number1) {
+      displayValue += num;
+      number1 = displayValue;
+      console.log(
+        "num input2",
+        num,
+        "n1",
+        number1,
+        "n2",
+        number2,
+        displayValue
+      );
     } else {
       displayValue += num;
+      number1 = displayValue;
+      console.log(
+        "num input5",
+        num,
+        "n1",
+        number1,
+        "n2",
+        number2,
+        displayValue
+      );
     }
-  } else {
+  } else if (displayValue === number1) {
     displayValue = num;
+    number2 = displayValue;
+    console.log(
+      "num input3",
+      num,
+      "n1: ",
+      number1,
+      "n2: ",
+      number2,
+      displayValue
+    );
+  } else {
+    displayValue += num;
+    number2 = displayValue;
+    console.log(
+      "num input4",
+      num,
+      "n1: ",
+      number1,
+      "n2: ",
+      number2,
+      displayValue
+    );
   }
 }
 
+// Currently not working correctly....
 function useOperators(operatorPressed) {
-  if (!display.innerText) {
+  if (display.innerText === "") {
     return;
   }
-  if (operationType !== null) {
-    if (display.innerText === null) {
-      operationType = operatorPressed;
-      console.log("op 1");
+  if (operation1 === null) {
+    if (number1 !== null && number2 === null) {
+      operation1 = operatorPressed;
+      console.log("opInput1: ", number1, operation1, number2);
       return;
     }
   } else {
-    operationType = operatorPressed;
-    display.innerText = "";
-    console.log("op 2");
+    operation2 = operatorPressed;
+    console.log("opInput2: ", number1, operation1, number2, operation2);
   }
 }
 
@@ -106,7 +159,10 @@ function inputAddDecimal(decimal) {
 function clearDisplay() {
   displayValue = "0";
   result = null;
-  operationType = null;
+  operation1 = null;
+  operation2 = null;
+  number1 = null;
+  number2 = null;
 }
 
 function inputEquals() {
