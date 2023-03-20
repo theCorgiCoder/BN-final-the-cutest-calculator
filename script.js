04f71e53f4a6a282bf3afca87cea8bc0495aad71
Elaine B <elainebergstrom26@gmail.com>
2023-03-20 18:59:15 +0100

    updated logic for updating solution for number/operator pairs without hitting equal

--- script.js
+++ script.js
@@ -12,13 +12,7 @@ let operation2 = null;
 function updateDisplay() {
   // display.innerText = parseFloat(displayValue).toFixed(6);
   display.innerText = displayValue;
-  console.log(
-    "UPDATE: ",
-    "Display: ",
-    display.innerText,
-    "displayValue: ",
-    displayValue
-  );
+  console.log("UPDATE: ", number1, operation1, number2, operation2);
 }
 onClick();
 //Listens for all button click events and then calls handler function
@@ -61,68 +55,27 @@ function onClickHandler(button) {
 function inputNumbers(num) {
   //handles first number input
   if (operation1 === null) {
-    if (displayValue === "0" || displayValue === 0) {
+    if (
+      displayValue === "0" ||
+      displayValue === 0 ||
+      displayValue === "Nope, stop it."
+    ) {
       displayValue = num;
       number1 = displayValue;
-      console.log(
-        "num input3",
-        num,
-        "n1: ",
-        number1,
-        "n2: ",
-        number2,
-        displayValue
-      );
     } else if (displayValue === number1) {
       displayValue += num;
       number1 = displayValue;
-      console.log(
-        "num input2",
-        num,
-        "n1",
-        number1,
-        "n2",
-        number2,
-        displayValue
-      );
     } else {
       displayValue += num;
       number1 = displayValue;
-      console.log(
-        "num input5",
-        num,
-        "n1",
-        number1,
-        "n2",
-        number2,
-        displayValue
-      );
     }
   } else if (displayValue === number1) {
     //handles second number input
     displayValue = num;
     number2 = displayValue;
-    console.log(
-      "num input3",
-      num,
-      "n1: ",
-      number1,
-      "n2: ",
-      number2,
-      displayValue
-    );
   } else {
     displayValue += num;
     number2 = displayValue;
-    console.log(
-      "num input4",
-      num,
-      "n1: ",
-      number1,
-      "n2: ",
-      number2,
-      displayValue
-    );
   }
 }
 
@@ -132,10 +85,32 @@ function useOperators(operatorPressed) {
   }
   if (operation1 === null) {
     operation1 = operatorPressed;
-    console.log("opInput1: ", number1, operation1, number2);
+  } else if (operation1 !== null && operation2 === null) {
+    updateResult(operatorPressed);
+  } else if (operation1 !== null && operation2 !== null) {
+    updateResult(operatorPressed);
   } else {
     operation2 = operatorPressed;
-    console.log("opInput2: ", number1, operation1, number2, operation2);
+  }
+}
+
+function updateResult(operatorPressed) {
+  // Updates result first pair of numbers and operator1
+  if (operation2 === null) {
+    operation2 = operatorPressed;
+    number2 = displayValue;
+    result = operate(Number(number1), Number(number2), operation1);
+    displayValue = result;
+    number1 = displayValue;
+    result = null;
+  } else {
+    // Updates every result after operation pressed
+    number2 = displayValue;
+    result = operate(Number(number1), Number(number2), operation2);
+    operation2 = operatorPressed;
+    displayValue = result;
+    number1 = displayValue;
+    result = null;
   }
 }
 
@@ -162,21 +137,30 @@ function inputPercentage() {
 }
 
 function clearDisplay() {
-  displayValue = "0";
-  result = null;
-  operation1 = null;
-  operation2 = null;
-  number1 = null;
-  number2 = null;
+  if (displayValue === "Nope, stop it.") {
+    result = null;
+    operation1 = null;
+    operation2 = null;
+    number1 = null;
+    number2 = null;
+  } else {
+    displayValue = "0";
+    result = null;
+    operation1 = null;
+    operation2 = null;
+    number1 = null;
+    number2 = null;
+  }
 }
 
 function inputEquals() {
-  if (operation1 !== null) {
-    result = operate(Number(number1), Number(number2), operation1);
-    number2 = displayValue;
-    if (result === "no") {
+  if (operation1 !== null && operation2 === null) {
+    if (result === "Nope, stop it.") {
       displayValue = "Nope, stop it.";
+      clearDisplay();
     } else {
+      result = operate(Number(number1), Number(number2), operation1);
+      number2 = displayValue;
       displayValue = result;
       number1 = displayValue;
       number2 = null;
@@ -184,12 +168,12 @@ function inputEquals() {
       operation2 = null;
       result = null;
     }
-  } else if (operation2 !== null) {
-    number2 = displayValue;
-    result = operate(Number(number1), Number(number2), operation1);
-    if (result === "no") {
+  } else if (operation1 !== null && operation2 !== null) {
+    if (result === "Nope, stop it.") {
       displayValue = "Nope, stop it.";
+      clearDisplay();
     } else {
+      result = operate(Number(number1), Number(number2), operation2);
       displayValue = result;
       number1 = displayValue;
       number2 = null;
@@ -211,7 +195,7 @@ function operate(n1, n2, op) {
     return n1 * n2;
   } else if (op === "÷") {
     if (n2 === 0) {
-      return "no";
+      return "Nope, stop it.";
     } else {
       return n1 / n2;
     }
